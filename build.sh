fc949da7d9187f34479e81ee13673b04756baac4
Astrism <ghelton@gmail.com>
2012-09-05 23:30:27 -0700

    changed casper install method

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 cd www
-brew install casperjs
+git clone git://github.com/n1k0/casperjs.git
+cd casperjs
+git checkout tags/1.0.0-RC1
+ln -sf `pwd`/bin/casperjs /usr/local/bin/casperjs
 npm install grunt
 grunt
 exit $?
