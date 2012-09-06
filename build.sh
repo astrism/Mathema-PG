8dfb8c14d8ac5a8731ee1f124ad9459cb21d4d59
Astrism <ghelton@gmail.com>
2012-09-06 00:23:00 -0700

    wrong folder for casperjs

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@ git clone git://github.com/n1k0/casperjs.git
 export PHANTOMJS_EXECUTABLE='phantomjs --local-to-remote-url-access=yes --ignore-ssl-errors=yes'
 export DISPLAY=:99.0
 sh -e /etc/init.d/xvfb start
-DISPLAY=:99.0 casperjs/bin/casperjs www/casperjs.js
+DISPLAY=:99.0 casperjs/bin/casperjs casperjs.js
 
 cd www
 npm install grunt
